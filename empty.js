d8fde6218f51c91e6a385e9f17fb18098905fdf3
Alexei <agontar@cybervisiontech.com>
2016-09-21 19:00:34 +0300

    Updated empty function

--- empty.js
+++ empty.js
@@ -1,8 +1,11 @@
-function empty(label) {
+function empty(label, debug) {
     return function () {
         console.log(label);
         Array.prototype.forEach.call(arguments, function (elem) {
             console.log(elem);
         });
-    }
+        if (debug) {
+            debugger;
+        }
+    };
 }
